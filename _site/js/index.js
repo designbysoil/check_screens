5b78050d42fb2aa52ce0cde54ba6aba229210411
Abdul Rehman Khawar <designbysoil@gmail.com>
2019-03-20 15:40:22 -0700

    Animation with Segments.

--- _site/js/index.js
+++ _site/js/index.js
@@ -1,7 +1,22 @@
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('bm'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: 'data.json'
-})
+var ring = document.getElementById('bm');
+
+var animData = {
+	wrapper: ring,
+	animType: 'svg',
+	loop: true,
+	prerender: false,
+	autoplay: false,
+	autoloadSegments: true,
+	path: 'data.json'
+};
+
+var anim = bodymovin.loadAnimation(animData);
+
+anim.addEventListener('DOMLoaded', function () {
+	anim.playSegments([[0, 50],[50, 0]], true);
+});
+
+$(ring).click(function () { // active state
+    anim.playSegments([[51, 80],[80, 51]], true);
+    $(ring).delay(2000).fadeOut(300);
+});
